fea51daa54e3ccd0daf151f168e6044fc3d9dca2
psychoxharsh-dev <psychoxharsh@gmail.com>
2026-05-26 17:53:03 +0530

    Add 'trial_payment_successful' to eventName checks

--- server.js
+++ server.js
@@ -611,7 +611,7 @@ app.get('/postback', async (req, res) => {
       amount = config.installAmt || 0;
       comment = config.installComment;
       addBalance = config.installBalance;
-    } else if (['trial', 'purchase', 'e2', 'complete', 'signup', 'goldbuy', 'sign_up_success', 'deposit', 'gold_silver_successful_purchase'].includes(eventName)) {
+    } else if (['trial', 'purchase', 'e2', 'complete', 'signup', 'goldbuy', 'sign_up_success', 'deposit', 'trial_payment_successful'].includes(eventName)) {
       amount = config.trialAmt || 0;
       comment = config.trialComment;
       addBalance = config.trialBalance;
