e8636dadaf22acb50f9719561b903ae42d99a613
psychoxharsh-dev <psychoxharsh@gmail.com>
2026-07-07 23:31:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const offerConfig = {
   'QuickTv Fire': { installAmt: 0.1, trialAmt: 20, installBalance: false, trialBalance: true, installComment: 'QuickTv Install', trialComment: 'QuickTv Trail' },
   'Muthoot': { installAmt: 0.1, trialAmt: 15, installBalance: false, trialBalance: true, installComment: 'Muthoot Install', trialComment: 'Muthoot Register' },
   'Incred Gold': { installAmt: 0.1, trialAmt: 22, installBalance: false, trialBalance: true, installComment: 'Incred Install', trialComment: 'Incred Gold' },
-  'StoryTv Fire': { installAmt: 0.1, trialAmt: 22, installBalance: false, trialBalance: true, installComment: 'StoryTv Install', trialComment: 'StoryTv Trail' }
+  'Story Max': { installAmt: 0.1, trialAmt: 25, installBalance: false, trialBalance: true, installComment: 'StoryMax Install', trialComment: 'StoryMax Trail' }
 };
 
 const rateLimitMap = {};
