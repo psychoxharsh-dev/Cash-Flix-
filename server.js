9431df04bf7da4935f1e0623a63b0321db5e6104
psychoxharsh-dev <psychoxharsh@gmail.com>
2026-07-07 15:28:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -662,7 +662,7 @@ app.get('/postback', async (req, res) => {
     let addBalance = false;
     const eventName = event?.trim().toLowerCase();
 
-    if (['web', 'initial', 'install', 'e1', 'default'].includes(eventName)) {
+    if (['web', 'initial', 'install', 'e1', 'Default'].includes(eventName)) {
       amount = config.installAmt || 0;
       comment = config.installComment;
       addBalance = config.installBalance;
