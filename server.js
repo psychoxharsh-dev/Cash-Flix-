a9b0fea34100ca353758e01c97f31d5e71f4840d
psychoxharsh-dev <psychoxharsh@gmail.com>
2026-07-08 22:33:15 +0530

    Add 'af_complete_registration' to eventName checks

--- server.js
+++ server.js
@@ -666,7 +666,7 @@ app.get('/postback', async (req, res) => {
       amount = config.installAmt || 0;
       comment = config.installComment;
       addBalance = config.installBalance;
-    } else if (['trial', 'purchase', 'e2', 'gold_buy', 'signup', 'register', 'registration', 'deposit', 'trial_payment_successful'].includes(eventName)) {
+    } else if (['trial', 'purchase', 'e2', 'gold_buy', 'signup', 'register', 'registration', 'deposit', 'af_complete_registration'].includes(eventName)) {
       amount = config.trialAmt || 0;
       comment = config.trialComment;
       addBalance = config.trialBalance;
